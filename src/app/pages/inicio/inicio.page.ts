078eb014ca1f1457db19009e0789bc477c8e0029
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2022-01-18 13:48:13 -0600

    fab (floating action button)

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -50,6 +50,11 @@ export class InicioPage implements OnInit {
       name: 'Date Time',
       redirectTo: '/date-time'
     },
+    {
+      icon: 'car-outline',
+      name: 'Fab',
+      redirectTo: '/fab'
+    },
   ];
 
 
